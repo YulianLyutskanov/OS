2bf37ed90d617adb26e565f1d2d36fabedfda247
Yulian Lyutskanov <julianbatka2003@gmail.com>
2025-04-07 10:45:57 +0300

    Update 2017-IN-03.sh

--- shell/2017-IN-03.sh
+++ shell/2017-IN-03.sh
@@ -2,4 +2,4 @@
 
  egrep --color 'Inf' ../sample-data/01-shell/2017-IN-03/example-passwd |\
  egrep --color '^[^:]*:[^:]*:[^:]*:[^:]*:[a-zA-Z ]*a[,:]' |\
- cut -d ':' -f 1 | sed -E 's/[^0-9]//g' | cut -c 2,3 | sort -r | uniq -c | tr -s ' ' | sort -n | head -n 1
+ cut -d ':' -f 1 | sed -E 's/[^0-9]//g' | cut -c 2,3 | sort | uniq -c | tr -s ' ' | sort -n -r | head -n 1
