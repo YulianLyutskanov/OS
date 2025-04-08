be31c9872ae62edaede0672e7c33b9d0309664d1
Yulian Lyutskanov <julianbatka2003@gmail.com>
2025-04-08 15:00:04 +0300

    Create 2016-SE-04.sh

---
+++ shell/shell-02/2016-SE-04.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [[ $# -ne 2 ]]; then
+    echo "script needs exactly 2 params"
+    exit 1;
+fi
+
+if [[ ( ! "${1}" =~ ^[0-9]{0,1}[0-9]*$ ) || ( ! "${2}" =~ ^[0-9]{0,1}[0-9]*$ ) ]]; then
+    echo "params must be positive numbers"
+    exit 2
+fi
+
+mkdir -p 'a'
+mkdir -p 'b'
+mkdir -p 'c'
+
+while read filename; do
+    wc=$(cat "${filename}" | wc -l)
+
+    if [[ wc -lt $1 ]]; then
+        mv "${filename}" ./a
+    elif [[ wc -lt $2  &&  wc -gt $1 ]]; then
+        mv "${filename}" ./b
+    else
+        mv "${filename}" ./c
+    fi
+
+done < <(find . -mindepth 0  -maxdepth 0 -type f)
