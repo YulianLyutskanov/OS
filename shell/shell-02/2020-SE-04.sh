dd82967d4d2b9f69529544be5ad9ee0b79339179
Yulian Lyutskanov <julianbatka2003@gmail.com>
2025-04-10 01:17:14 +0300

    Create 2020-SE-04.sh

---
+++ shell/shell-02/2020-SE-04.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [[ $# -ne 2 ]]; then
+    echo " 2 args needed"
+    exit 1
+fi
+
+if [[ (! -d "${1}" ) || ( -e "${2}" ) ]]; then
+    echo "first arg must be an existing dir, and the second is created from the script dir"
+    exit 2
+fi
+
+while read -d $'\0' pic; do
+    if [[ -z  $( echo "${pic}" | egrep "\.jpg$" ) ]]; then
+        continue;
+    fi
+
+    title=$( echo "${pic}" | xargs -I {} basename "{}" | sed -E "s/.jpg$//g" | sed -E "s/\([^\(\)]*\)//g" |\
+    tr -s ' ' | sed -E "s/^ //g" | sed -E "s/ $//g" )
+
+    album=$( echo "${pic}" | xargs -I {} basename "{}" | sed -E "s/.jpg$//g" | sed -E "s/.*(\([^\(\)]*\))[^\(\)]*/\1/g" |\
+    sed -E "s/^[^\(\)]*$/misc/g" | sed -E "s/\(//g" | sed -E "s/\)//g" )
+
+    date=$( stat "${pic}" --printf "%y\n" | cut -d ' ' -f 1 )
+
+    cs=$( sha256sum "${pic}" | cut -c 1-16 )
+
+    mkdir -p "${2}/images"
+    cp "${pic}" "${2}/images/$cs.jpg"
+
+    mkdir -p "${2}/by-date/$date/by-album/$album/by-title"
+    ln -s "${2}/images/$cs.jpg" "${2}/by-date/$date/by-album/$album/by-title/$title.jpg"
+
+    mkdir -p "${2}/by-date/$date/by-title"
+    ln -s "${2}/images/$cs.jpg" "${2}/by-date/$date/by-title/$title.jpg"
+
+    mkdir -p "${2}/by-album/$album/by-date/$date/by-title"
+    ln -s "${2}/images/$cs.jpg" "${2}/by-album/$album/by-date/$date/by-title/$title.jpg"
+
+    mkdir -p "${2}/by-album/$album/by-title"
+    ln -s "${2}/images/$cs.jpg" "${2}/by-album/$album/by-title/$title.jpg"
+
+    mkdir -p "${2}/by-title"
+    ln -s "${2}/images/$cs.jpg" "${2}/by-title/$title.jpg"
+
+done < <(find "${1}"  -type f -print0)
